0f96ccf20ef8c2750b3c01d1c6bc0e92f15fee1b
Rodrigo Lourenço <lourenco.rs@gmail.com>
2019-10-17 14:16:19 -0300

    Estrutura configurada

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,59 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es6: true,
+    jest: true,
+    browser: true
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier',
+    'react-hooks'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'no-console': [
+      'error',
+      { allow: ['tron'] }
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    camelcase: 'off',
+    'no-underscore-dangle': 'off',
+    'no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_' }
+    ]
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: "src"
+      }
+    }
+  }
 };
---
+++ src/store/modules/auth/reducer.js
@@ -0,0 +1,7 @@
+const INITIAL_STATE = {
+  signed: false,
+};
+
+export default function auth(state = INITIAL_STATE, action) {
+  return INITIAL_STATE;
+}
