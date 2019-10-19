ad8c77cc7378387a2f676642b2827f315dc51f39
Rodrigo Lourenço <lourenco.rs@gmail.com>
2019-10-19 13:41:09 -0300

    Rota inicial

---
+++ src/routes.js
@@ -0,0 +1,38 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Platform } from 'react-native';
+
+import SignIn from './pages/auth/SignIn';
+import SignUp from './pages/auth/SignUp';
+import Dashboard from './pages/Dashboard';
+
+export default isSigned =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          SignIn,
+          SignUp,
+        }),
+        App: createBottomTabNavigator(
+          {
+            Dashboard,
+          },
+          {
+            resetOnBlur: true,
+            tabBarOptions: {
+              keyboardHidesTabBar: Platform.OS === 'ios',
+              activeTintColor: '#FFF',
+              inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+              style: {
+                backgroundColor: '#2B1A2F',
+              },
+            },
+          }
+        ),
+      },
+      {
+        initialRouteName: isSigned ? 'App' : 'Sign',
+      }
+    )
+  );
