69e4c0aed3f320a181f718faa387fe2c50a151c1
Rodrigo Lourenço <lourenco.rs@gmail.com>
2019-10-23 09:23:01 -0300

    Inclusão dos ícones nas informações do meetup

--- src/components/Meetup/index.js
+++ src/components/Meetup/index.js
@@ -4,7 +4,15 @@ import PropTypes from 'prop-types';
 import Button from '~/components/Button';
 import banner from '~/assets/banner.png';
 
-import { Container, Content, Title, Text, Banner } from './styles';
+import {
+  Container,
+  Content,
+  Title,
+  Field,
+  Icon,
+  FieldText,
+  Banner,
+} from './styles';
 
 export default function Meetup({ data }) {
   return (
@@ -12,9 +20,20 @@ export default function Meetup({ data }) {
       <Banner source={banner} />
       <Content>
         <Title>{data.title}</Title>
-        <Text>{data.dateFormatted}</Text>
-        <Text>{data.location}</Text>
-        <Text>Organizador: {data.User.name}</Text>
+
+        <Field>
+          <Icon name="event" />
+          <FieldText>{data.dateFormatted}</FieldText>
+        </Field>
+        <Field>
+          <Icon name="place" />
+          <FieldText>{data.location}</FieldText>
+        </Field>
+        <Field>
+          <Icon name="person" />
+          <FieldText>Organizador: {data.User.name}</FieldText>
+        </Field>
+
         <Button>Realizar inscrição</Button>
       </Content>
     </Container>
--- src/components/Meetup/styles.js
+++ src/components/Meetup/styles.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components/native';
 
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+
 export const Container = styled.View`
   background: #fff;
   border-radius: 4px;
@@ -14,6 +16,10 @@ export const Banner = styled.Image.attrs({
   width: 100%;
 `;
 
+export const Content = styled.View`
+  padding: 20px;
+`;
+
 export const Title = styled.Text`
   font-size: 18px;
   font-weight: bold;
@@ -21,12 +27,20 @@ export const Title = styled.Text`
   margin-bottom: 15px;
 `;
 
-export const Content = styled.View`
-  padding: 20px;
+export const Field = styled.View`
+  flex-direction: row;
+  align-items: center;
+
+  margin-bottom: 15px;
 `;
 
-export const Text = styled.Text`
+export const Icon = styled(MaterialIcon).attrs({
+  color: '#999',
+})`
+  margin-right: 6px;
+`;
+
+export const FieldText = styled.Text`
   font-size: 13px;
   color: #999;
-  margin: 0 0 15px 20px;
 `;
