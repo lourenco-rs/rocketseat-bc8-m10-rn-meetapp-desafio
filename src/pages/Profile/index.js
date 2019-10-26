c02db417ce1bf3d9dee62af7aa64c7582ae89f85
Rodrigo Lourenço <lourenco.rs@gmail.com>
2019-10-26 20:11:11 -0300

    Atualização de perfil e logout

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,17 +1,121 @@
-import React from 'react';
-import { Text } from 'react-native';
-
+import React, { useRef, useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import Background from '~/components/Background';
 import Header from '~/components/Header';
 
-// import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+import { signOut } from '~/store/modules/auth/actions';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  Separator,
+  SubmitButton,
+  LogoutButton,
+} from './styles';
 
 export default function Profile() {
+  const dispatch = useDispatch();
+  const { profile } = useSelector(state => state.user);
+
+  const emailRef = useRef();
+  const oldPasswordRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+
+  const [name, setName] = useState(profile.name);
+  const [email, setEmail] = useState(profile.email);
+  const [oldPassword, setOldPassword] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  useEffect(() => {
+    setOldPassword('');
+    setPassword('');
+    setConfirmPassword('');
+  }, [profile]);
+
+  function handleSubmit() {
+    dispatch(
+      updateProfileRequest({
+        name,
+        email,
+        oldPassword,
+        password,
+        confirmPassword,
+      })
+    );
+  }
+
+  function handleLogout() {
+    dispatch(signOut());
+  }
+
   return (
     <Background>
       <Header />
-      <Text>Meu perfil</Text>
+      <Container>
+        <Form>
+          <FormInput
+            autoCorrect={false}
+            placeholder="Nome completo"
+            returnKeyType="next"
+            onSubmitEditing={() => emailRef.current.focus()}
+            value={name}
+            onChangeText={setName}
+          />
+
+          <FormInput
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+            ref={emailRef}
+            returnKeyType="next"
+            onSubmitEditing={() => oldPasswordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
+          />
+
+          <Separator />
+
+          <FormInput
+            secureTextEntry
+            placeholder="Sua senha atual"
+            ref={oldPasswordRef}
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+            value={oldPassword}
+            onChangeText={setOldPassword}
+          />
+
+          <FormInput
+            secureTextEntry
+            placeholder="Sua nova senha"
+            ref={passwordRef}
+            returnKeyType="next"
+            onSubmitEditing={() => confirmPasswordRef.current.focus()}
+            value={password}
+            onChangeText={setPassword}
+          />
+
+          <FormInput
+            secureTextEntry
+            placeholder="Confirmação de senha"
+            ref={confirmPasswordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+          />
+
+          <SubmitButton onPress={handleSubmit}>Salvar perfil</SubmitButton>
+          <LogoutButton onPress={handleLogout}>Sair do Meetapp</LogoutButton>
+        </Form>
+      </Container>
     </Background>
   );
 }
---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components/native';
+
+import Input from '~/components/Input';
+import Button from '~/components/Button';
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+`;
+
+export const Form = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    showsVerticalScrollIndicator: false,
+    padding: 30,
+  },
+})`
+  align-self: stretch;
+`;
+
+export const FormInput = styled(Input)`
+  margin-bottom: 10px;
+`;
+
+export const Separator = styled.View`
+  height: 1px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 20px 0 30px;
+`;
+
+export const SubmitButton = styled(Button)`
+  margin-top: 5px;
+  background: #e5556e;
+`;
+
+export const LogoutButton = styled(Button)`
+  height: 42px;
+  margin-top: 15px;
+  background: #d44059;
+`;
